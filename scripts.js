e48e10656ccd9302ebac8ef0ae7acbc717a2c3f6
Andrey Lyamkin <andreylyamkin@Andreys-MacBook-Air.local>
2023-06-10 18:10:15 -0400

    Add functions code spipets

--- scripts.js
+++ scripts.js
@@ -153,26 +153,135 @@
 /**
  * The bind() method
  */
-const sum = function () {
-  return this.x + this.y;
+// const sum = function () {
+//   return this.x + this.y;
+// };
+
+// const obj1 = {
+//   x: 1,
+//   y: 2,
+// };
+
+// const obj2 = {
+//   x: 2,
+//   y: 3,
+// };
+
+// const obj3 = {
+//   x: 4,
+//   y: 5,
+// };
+
+// const sumObj1 = sum.bind(obj1);
+// const sumObj2 = sum.bind(obj2);
+
+// obj3.sum = sumObj2;
+
+/**
+ * Examples
+ */
+
+// 8.1.4 Nested Functions
+
+/*function hypotenuse(a, b) {
+  const square = (x) => x * x;
+  return Math.sqrt(square(a) + square(b));
+}
+
+const calcHypotenuse = hypotenuse(2, 2);
+console.log(calcHypotenuse);*/
+
+// 8.2.2 Method Invocation
+
+/*let o = {
+  m: function () {
+    let self = this;
+    // function f() {
+    //   console.log(this === o);
+    //   console.log(self === o);
+    // }
+
+    // const f = () => {
+    //   console.log(this === o);
+    // };
+
+    const f = function () {
+      console.log(this === o);
+    }.bind(this);
+    f();
+  },
 };
 
-const obj1 = {
-  x: 1,
-  y: 2,
+o.m();*/
+
+// 8.3.4 The Spread Operator for Function Call
+
+/*function timed(f) {
+  return function (...args) {
+    console.log(`Entered function ${f.name}`);
+    let startTime = new Date();
+    try {
+      return f(...args);
+    } finally {
+      console.log(`Exiting ${f.name} after ${new Date() - startTime}ms `);
+    }
+  };
+}
+
+function benchmark(n) {
+  let sum = 0;
+  for (let i = 0; i < n; i++) {
+    sum += i;
+  }
+
+  return sum;
+}
+
+console.log(timed(benchmark)(1000000000));*/
+
+// 8.3.5 Destructuring Function Arguments into Paraterters
+
+/*function vectorMultiply({ x, y }, scalar) {
+  return { x: x * scalar, y: y * scalar };
+}
+
+console.log(vectorMultiply({ x: 2, y: 3 }, 2));*/
+
+/*function vectorAdd({ x: x1, y: y1 }, { x: x2, y: y2 }) {
+  return { x: x1 + x2, y: y1 + y2 };
+}
+
+console.log(vectorAdd({ x: 2, y: 4 }, { x: 3, y: 5 }));*/
+
+// 8.8.2 Higher-Order Function
+
+/*const map = (args, callback) => {
+  return args.map(callback);
 };
 
-const obj2 = {
-  x: 2,
-  y: 3,
-};
+const increment = (x) => x + 1;
 
-const obj3 = {
-  x: 4,
-  y: 5,
-};
+const powerTwo = (x) => x * x;
 
-const sumObj1 = sum.bind(obj1);
-const sumObj2 = sum.bind(obj2);
+function mapper(f) {
+  return (a) => map(a, f);
+}
 
-obj3.sum = sumObj2;
+const incrementAll = mapper(increment);
+const powerTwoAll = mapper(powerTwo);
+
+console.log(incrementAll([1, 2, 3, 4]));
+console.log(powerTwoAll([1, 2, 3, 4]));*/
+
+//Returned a new function that computes f(g())
+
+/*const sum = (x, y) => x + y;
+const square = (x) => x * x;
+
+function compose(f, g) {
+  return function (...args) {
+    return f.call(this, g.apply(this, args));
+  };
+}
+
+console.log(compose(square, sum)(3, 6));*/
